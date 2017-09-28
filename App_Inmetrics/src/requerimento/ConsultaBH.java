20ef89df63e1ed56639a1798c863095b5d375817
Ademir <ademir_rg@yahoo.com.br>
2017-09-28 00:04:06 -0300

    ...

--- App_Inmetrics/src/requerimento/ConsultaBH.java
+++ App_Inmetrics/src/requerimento/ConsultaBH.java
@@ -30,32 +30,60 @@ public class ConsultaBH {
 		pass.sendKeys(cpf);
 		WebElement btContinuar = driver.findElement(By.id("btnSubmitLogn"));
 		btContinuar.click();
-		//Pega valores
-		WebElement table = driver.findElement(By.xpath("//*[@id=\"frequencia\"]/tbody/tr[2]/td/table[1]"));
-		String saldoBH  = table.findElement(By.xpath("//*[@id=\"frequencia\"]/tbody/tr[2]/td/table[1]/tbody/tr[9]/td")).getText();
+		
+		//Saldo do BH
+		WebElement tabelaSaldo = driver.findElement(By.xpath("//*[@id=\"frequencia\"]/tbody/tr[2]/td/table[1]"));
+		String saldoBH  = tabelaSaldo.findElement(By.xpath("//*[@id=\"frequencia\"]/tbody/tr[2]/td/table[1]/tbody/tr[9]/td")).getText();
 		String saldoBHSeparado[] = saldoBH.split(":");
 		saldoBHSeparado[1] = saldoBHSeparado[1].replaceFirst("0", "");
 		saldoBHSeparado[1] = saldoBHSeparado[1].replaceFirst(" ", "");
 		saldoBH = saldoBHSeparado[1] + ":" + saldoBHSeparado[2];
 		saldoBH.trim();
+		float horaBH = Float.parseFloat(saldoBHSeparado[1]);
+		float minutosBH = Float.parseFloat(saldoBHSeparado[2]);
 		//saldoBH = "22:00";
+		
+		//T.HE
+		String saldoHoraExtra = driver.findElement(By.xpath("//*[@id=\"tbDivergencias\"]/tbody/tr/td[5]")).getText();
+		String saldoHoraExtraSep[] = saldoHoraExtra.split(":");
+		float horaExtra = Float.parseFloat(saldoHoraExtraSep[0]);
+		float minutosExtra = Float.parseFloat(saldoHoraExtraSep[1]);
+		
+		//T.Atrasos
+		String saldoAtraso = driver.findElement(By.xpath("//*[@id=\"tbDivergencias\"]/tbody/tr/td[6]")).getText();
+		String saldoAtrasoSep[] = saldoAtraso.split(":");
+		float horaAtraso = Float.parseFloat(saldoAtrasoSep[0]);
+		float minutosAtraso = Float.parseFloat(saldoAtrasoSep[1]);
+		
+		//T.Faltas
+		String saldoFaltas = driver.findElement(By.xpath("//*[@id=\"tbDivergencias\"]/tbody/tr/td[7]")).getText();
+		String saldoFaltasSep[] = saldoFaltas.split(":");
+		float horaFalta = Float.parseFloat(saldoFaltasSep[0]);
 		
 		if(saldoBH.startsWith("-")){
 			System.out.println("Numero negativo");
-			float hora = Float.parseFloat(saldoBHSeparado[1]);
-			float minutos = Float.parseFloat(saldoBHSeparado[2]);
-			hora = hora * 60;
-			hora = hora - minutos;
-			System.out.println(hora);
+			horaBH = horaBH * 60;
+			horaBH = horaBH - minutosBH;
+			horaExtra = horaExtra * 60 + minutosExtra;
+			horaBH = horaBH + horaExtra;
+			horaAtraso = horaAtraso * 60 + minutosAtraso;
+			horaBH = horaBH - horaAtraso;
+			horaFalta = horaFalta * 60;
+			horaBH = horaBH - horaFalta;
+			System.out.println(horaBH/60);
 		}
 		
 		else{
 			System.out.println("Numero positivo");
-			float hora = Float.parseFloat(saldoBHSeparado[1]);
-			float minutos = Float.parseFloat(saldoBHSeparado[2]);
-			hora = hora * 60;
-			hora = hora + minutos;
-			System.out.println(hora);
+			horaBH = horaBH * 60;
+			horaBH = horaBH + minutosBH;
+			horaExtra = horaExtra * 60 + minutosExtra;
+			horaBH = horaBH + horaExtra;
+			horaAtraso = horaAtraso * 60 + minutosAtraso;
+			horaBH = horaBH - horaAtraso;
+			horaFalta = horaFalta * 60;
+			horaBH = horaBH - horaFalta;
+			System.out.println(horaBH/60);
 		}
 			
 		
