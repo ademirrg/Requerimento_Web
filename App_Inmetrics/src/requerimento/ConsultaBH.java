73e42917f978274c9a8673d7dc35e289c7d302ae
rogarcia <rogarcia@LP1045>
2017-12-07 19:12:29 -0200

    Ajustes na validação de login

--- App_Inmetrics/src/requerimento/ConsultaBH.java
+++ App_Inmetrics/src/requerimento/ConsultaBH.java
@@ -49,17 +49,18 @@ public class ConsultaBH {
 	
 	public void abreBrowser(){
 		try{
-		System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
-		driver = new ChromeDriver();
-		driver.manage().window().maximize();
-		driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+			System.setProperty("webdriver.chrome.driver","C:\\chromedriver.exe");
+			driver = new ChromeDriver();
+			driver.manage().window().maximize();
+			driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
 		}
 		catch(IllegalStateException e){
 			JOptionPane.showMessageDialog(null, "Ops!\nParece que você não tem o chromedriver em sua máquina!"
 					+ "\nPara utilizar esta aplicação, baixe a versão mais recente do chromedriver no link abaixo:"
 					+ "\n<html><span style ='color:blue'>https://sites.google.com/a/chromium.org/chromedriver/downloads</span></html>"
 					+ "\nApós baixar o arquivo chromedriver.exe, coloque-o no diretório C: e execute a aplicação."
-					+ "\nClique em OK e o link será copiado para sua área de transferência, após isso, apenas cole (Ctrl+V) na página do seu navegador.", "ATENÇÃO", JOptionPane.WARNING_MESSAGE);	
+					+ "\nClique em OK e o link será copiado para sua área de transferência, após isso, "
+					+ "apenas cole (Ctrl+V) na página do seu navegador.", "ATENÇÃO", JOptionPane.WARNING_MESSAGE);	
 			String link = "https://sites.google.com/a/chromium.org/chromedriver/downloads";
 			StringSelection selectlLink = new StringSelection(link);
 			Clipboard areaTransfer = Toolkit.getDefaultToolkit().getSystemClipboard();
@@ -73,13 +74,28 @@ public class ConsultaBH {
 	}
 	
 	public void login(){
-		driver.get("https://aplic.inmetrics.com.br//requerimento/content/login.php");
-		WebElement user = driver.findElement(By.id("fun_Id"));
-		user.sendKeys(cpf);
-		WebElement pass = driver.findElement(By.id("fun_Senha"));
-		pass.sendKeys(cpf);
-		WebElement btContinuar = driver.findElement(By.id("btnSubmitLogn"));
-		btContinuar.click();	
+		try{
+			driver.get("https://aplic.inmetrics.com.br//requerimento/content/login.php");
+			WebElement user = driver.findElement(By.id("fun_Id"));
+			user.sendKeys(cpf);
+			WebElement pass = driver.findElement(By.id("fun_Senha"));
+			pass.sendKeys(cpf);
+			WebElement btContinuar = driver.findElement(By.id("btnSubmitLogn"));
+			btContinuar.click();	
+			boolean userInvalido = driver.getPageSource().contains("Problemas! Usuário ou senha divergente!");
+			if(userInvalido == true){
+				driver.quit();
+				JOptionPane.showMessageDialog(null, "Problemas ao realizar login!\nVerifique se o CPF informado está correto: " 
+						+ cpf, "ERRO", JOptionPane.ERROR_MESSAGE);
+				System.exit(0);
+			}
+		}
+		catch(NoSuchElementException e){
+			driver.quit();
+			JOptionPane.showMessageDialog(null, "Erro ao processar solicitação de login!"
+					+ "\nProblemas de conexão com a internet ou elemento não encontrado na página.", "ERRO", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
 	}
 		
 	public void consultaHoras(){	
@@ -106,8 +122,8 @@ public class ConsultaBH {
 		}
 		catch(NoSuchElementException e){
 			driver.quit();
-			JOptionPane.showMessageDialog(null, "Problemas ao realizar login!\nVerifique se o CPF informado está correto: " 
-			+ cpf, "ERRO", JOptionPane.ERROR_MESSAGE);
+			JOptionPane.showMessageDialog(null, "Erro ao processar solicitação de consulta do banco de horas!"
+					+ "\nProblemas de conexão com a internet ou elemento não encontrado na página.", "ERRO", JOptionPane.ERROR_MESSAGE);
 			System.exit(0);
 		}
 		
@@ -271,27 +287,34 @@ public class ConsultaBH {
 				saldoFinal = horaFinal + ":" + minutosFinal;
 				JOptionPane.showMessageDialog(null, "Seu saldo: " + 
 						saldoFinal, "Consulta Banco de Horas", JOptionPane.INFORMATION_MESSAGE);
-			}
-			
+			}	
 		}
 	}
 	
 	public void consultaPeriodoAnterior(){
-		//Altera período
-		WebElement altPeriodo = driver.findElement(By.xpath("//*[@id=\"frequencia\"]/tbody/tr[3]/td/input[2]"));
-		altPeriodo.click();
-		GregorianCalendar cal = new GregorianCalendar();
-		int mes = cal.get(Calendar.MONTH);
-		int ano = cal.get(Calendar.YEAR);
-		Integer.toString(mes);
-		Integer.toString(ano);
-		String mesPeriodo = mes + "/" + ano;
-		Select cbPeriodo = new Select(driver.findElement(By.name("refCompetencia")));
-		cbPeriodo.selectByValue(mesPeriodo);
-		WebElement btAltPeriodo = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td/table/tbody/tr[3]/td/input[1]"));
-		btAltPeriodo.click();
-		consultaHoras();
-		driver.quit();
-		calculaHoras();
+		try{
+			//Altera período
+			WebElement altPeriodo = driver.findElement(By.xpath("//*[@id=\"frequencia\"]/tbody/tr[3]/td/input[2]"));
+			altPeriodo.click();
+			GregorianCalendar cal = new GregorianCalendar();
+			int mes = cal.get(Calendar.MONTH);
+			int ano = cal.get(Calendar.YEAR);
+			Integer.toString(mes);
+			Integer.toString(ano);
+			String mesPeriodo = mes + "/" + ano;
+			Select cbPeriodo = new Select(driver.findElement(By.name("refCompetencia")));
+			cbPeriodo.selectByValue(mesPeriodo);
+			WebElement btAltPeriodo = driver.findElement(By.xpath("/html/body/form/table/tbody/tr[2]/td/table/tbody/tr[3]/td/input[1]"));
+			btAltPeriodo.click();
+			consultaHoras();
+			driver.quit();
+			calculaHoras();
+		}
+		catch(NoSuchElementException e){
+			driver.quit();
+			JOptionPane.showMessageDialog(null, "Erro ao processar solicitação de alteração de período!"
+					+ "\nProblemas de conexão com a internet ou elemento não encontrado na página.", "ERRO", JOptionPane.ERROR_MESSAGE);
+			System.exit(0);
+		}
 	}
 }
